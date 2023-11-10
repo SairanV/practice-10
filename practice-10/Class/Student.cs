61c22d3933f3b5bd48cb2c70f3a08ec9e1278f74
aidyn <aidynmaskutov2004@gmail.com>
2023-11-10 23:25:54 +0600

    Добавьте файлы проекта.

---
+++ practice-10/Class/Student.cs
@@ -0,0 +1,40 @@
+﻿using practice_10.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_10.Class
+{
+    public class Student : IStudent
+    {
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public int[] Grades { get; set; }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetFullName()
+        {
+            return FullName;
+        }
+
+        public double GetAvgGrade()
+        {
+            if (Grades.Length == 0)
+                return 0;
+
+            int sum = 0;
+            foreach (var grade in Grades)
+            {
+                sum += grade;
+            }
+
+            return (double)sum / Grades.Length;
+        }
+    }
+}
---
+++ practice-10/Program.cs
@@ -0,0 +1,27 @@
+﻿using practice_10.Class;
+using practice_10.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IStudent student = new Student
+            {
+                Name = "Айдонт",
+                FullName = "Айдонт Непон",
+                Grades = new int[] { 80, 101, 75, 88, 100 } // | C# | Музыка | Физкультура | Java | Просмотр YouTube |
+            };
+
+            Console.WriteLine($"Имя студента: {student.GetName()}");
+            Console.WriteLine($"Полное имя студента: {student.GetFullName()}");
+            Console.WriteLine($"Средний балл студента: {student.GetAvgGrade()}");
+        }
+    }
+}
